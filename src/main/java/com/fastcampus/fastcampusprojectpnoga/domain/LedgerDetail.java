c66cc7132753d7066e074ac09230bb37df15dfcb
msk102948 <msk102948@gmail.com>
2024-03-27 10:59:12 +0900

    #8 - 도메인 클래스 설계(가계부, 세부사항)
    도메인 기본 정보 설계.
    JPA가 적용 되면 기존 도메인으로 적용 예정.

---
+++ src/main/java/com/fastcampus/fastcampusprojectpnoga/domain/LedgerDetail.java
@@ -0,0 +1,8 @@
+package com.fastcampus.fastcampusprojectpnoga.domain;
+
+public class LedgerDetail {
+    private String id;
+    private String ledger_id; // 가계부 id
+    private String content; // 내용
+    private float budget; // 금액
+}
