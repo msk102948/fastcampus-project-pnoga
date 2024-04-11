112b135d280bde830b689a0b4b355f9b8e818060
msk102948 <msk102948@gmail.com>
2024-04-11 21:02:27 +0900

    #9 - 도메인 코드를 엔티티로 변환
    1 도메인 == 1 테이블 매칭 되도록 설계했다.
    BaseEntity도 모두 설정함
    생성자와 equals(), hashcode() 등이 도메인 맥락에 맞게끔 디테일하게 설정(=> 이건 BaseEntity에 설정함)

--- src/main/java/com/fastcampus/fastcampusprojectpnoga/domain/Ledger.java
+++ src/main/java/com/fastcampus/fastcampusprojectpnoga/domain/Ledger.java
@@ -1,8 +1,28 @@
 package com.fastcampus.fastcampusprojectpnoga.domain;
 
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.time.LocalDate;
 import java.util.Date;
 
-public class Ledger {
-    private String id;
-    private Date baseDate; // 기준 일자
+@Getter
+@ToString
+@Entity
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Ledger extends BaseEntity{
+
+    @Column
+    @Comment("기준일자")
+    @Setter
+    private LocalDate baseDate;
+
+    public static Ledger of(LocalDate baseDate){
+        return new Ledger(baseDate);
+    }
+
+
 }
